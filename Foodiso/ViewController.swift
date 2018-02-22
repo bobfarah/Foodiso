4ecc8dbd7c3662a163db6782aa36bd7d9f397d89
Babak Farahanchi <babakfarahanchi@icloud.com>
2018-02-22 14:58:17 -0500

    IBM Watson calling

--- Foodiso/ViewController.swift
+++ Foodiso/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+ 
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+        }else{
+            print("issue in taking photo bobby jan")
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+        
+        
     }
-
-
+    
 }
 
